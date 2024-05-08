9dd9180ceccb91bb9d67b6263e58cf0990cef7df
Sabil667 <msabi@myuax.com>
2024-05-08 09:53:10 +0100

    .

--- src/main/java/EJERCICIO1/Tweet.java
+++ src/main/java/EJERCICIO1/Tweet.java
@@ -1,4 +1,13 @@
 package EJERCICIO1;
 
 public class Tweet {
+    private String content;
+    private UserAccount user;
+    private Date date;
+
+    public Tweet(String content, UserAccount user) {
+        this.content = content;
+        this.user = user;
+        this.date = new Date();
+    }
 }
