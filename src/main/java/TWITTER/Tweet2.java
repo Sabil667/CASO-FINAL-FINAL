c3eeb5fb4c3fe8850b7b9ff732682abedb7a8028
Sabil667 <msabi@myuax.com>
2024-05-08 11:23:11 +0100

    .

--- src/main/java/TWITTER/Tweet2.java
+++ src/main/java/TWITTER/Tweet2.java
@@ -1,4 +1,27 @@
 package TWITTER;
 
+import java.time.LocalDate;
+
 public class Tweet2 {
+    protected String message;
+    protected UserAccount sender;
+    protected LocalDate time;
+
+    public Tweet2(String message, UserAccount sender) {
+        if (message.length() > 140) {
+            throw new IllegalArgumentException("El mensaje no puede tener más de 140 caracteres");
+        }
+        this.message = message;
+        this.sender = sender;
+        this.time = LocalDate.now();
+    }
+
+    @Override
+    public String toString() {
+        return "Tweet2{" +
+                "message='" + message + '\'' +
+                ", sender=" + sender +
+                ", time=" + time +
+                '}';
+    }
 }
